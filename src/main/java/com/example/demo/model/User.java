46c13c9c56ff3f874786bfb675ea8162f26fdcb3
gebustosc <ge.bustosc@duocuc.cl>
2026-05-10 23:20:20 -0400

    test

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -24,7 +24,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUser;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false,unique=true)
     private String email;
 
     @Column(name = "password", nullable = false)
